e0ce46da9f052fb528091e3e6b7beeab463ccd64
Christopher Brooks <flavenstein@gmail.com>
2017-03-10 13:44:46 -0700

    Redex-II search is now linear

--- semiunification.scm
+++ semiunification.scm
@@ -1,9 +1,11 @@
-(define var vector)
-(define var? vector?)
-(define (var=? x1 x2) (= (vector-ref x1 0) (vector-ref x2 0)))
+(load "parse.scm")
+(load "examples.scm")
+
+(define (arity-0? term)
+  (and (list? term) (= (length term) 1) (atom? (car term))))
 
 (define (flatten term path)
-  (cond [(or (var? term) (and (pair? term) (null? (cdr term))))
+  (cond [(or (var? term) (arity-0? term))
          (list (cons term path))]
         [else
          (let loop ([args (cdr term)] [i 0])
@@ -11,9 +13,24 @@
                (append (flatten (car args) (append path (list (cons (car term) i))))
                        (loop (cdr args) (+ i 1)))))]))
 
-(define (paths SUP)
-  (values (flatten (car SUP) '())
-          (flatten (cdr SUP) '())))
+(define (path-sort ps)
+  (sort
+    (lambda (x y)
+      (< (vector-ref (car x) 0)
+         (vector-ref (car y) 0)))
+    ps))
+
+(define (paths ineq)
+  (match ineq
+    [(< ,lhs ,rhs)
+     `(< ,(path-sort (flatten lhs '()))
+         ,(path-sort (flatten rhs '())))]))
+
+(define (explode SUP)
+  (match SUP
+    [() '()]
+    [(,ineq . ,ineqs)
+     (cons (paths ineq) (explode ineqs))]))
 
 (define (resolve-path flat-term path)
   (if (null? path)
@@ -40,24 +57,10 @@
                (cons (caar rhs) (freshen lhs))))]
         [else (redex-I lhs (cdr rhs))]))
 
-(define (redex-II lhs rhs)
-  (let loop1 ([lhs lhs])
-    (if (null? (cdr lhs)) #f
-        (let loop2 ([rest (cdr lhs)])
-          (if (null? rest) (loop1 (cdr lhs))
-              (let ([t1 (caar lhs)]
-                    [t2 (caar rest)])
-                (if (and (var? t1) (var? t2) (var=? t1 t2))
-                    (let ([p1 (resolve-path rhs (cdar lhs))]
-                          [p2 (resolve-path rhs (cdar rest))])
-                      (if (equal? p1 p2) (loop2 (cdr rest))
-                          (or (unify p1 p2) (loop2 (cdr rest)))))
-                    (loop2 (cdr rest)))))))))
-
-(define (redex SUP)
-  (if (null? SUP) '()
-      (let ([ineq (car SUP)])
-        (cons (cond [(redex-I? ineq) (reduce-I ineq)]
-                    [(redex-II? ineq) (reduce-II ineq)]
-                    [else ineq])
-              (redex (cdr SUP))))))
+(define (redex-II-2 ineq)
+  (match ineq
+    [(< ,lhs ,rhs)
+      (match lhs
+        [(,a) #f]
+        [((,a . ,p1) (,a . ,p2) . ,tail) (cons p1 p2)]
+        [,_ (redex-II-2 `(< ,(cdr lhs) ,rhs))])]))
