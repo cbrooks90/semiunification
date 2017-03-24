1c4eb512dfb7273497cbf05b2b29842968ea544b
Christopher Brooks <flavenstein@gmail.com>
2017-03-24 14:44:36 -0600

    Finish redex II

--- semiunification.scm
+++ semiunification.scm
@@ -3,24 +3,22 @@
 (define (freshen x) x)
 
 (define (redex-args l-args r-args visited)
-  (cond
-    [(and (null? l-args) (null? r-args)) (values #f visited)]
-    [(or (null? l-args) (null? r-args)) (error #f "Arity mismatch")]
-    [else
-     (let-values ([(subst visited~) (redex-term (car l-args) (car r-args))])
-       (if subst
-           (values subst #f)
-           (redex-args (cdr l-args) (cdr r-args))))]))
+  (if (or (null? l-args) (null? r-args)) (values #f visited)
+      (let-values ([(subst visited~) (redex-term (car l-args) (car r-args))])
+        (if subst
+            (values subst #f)
+            (redex-args (cdr l-args) (cdr r-args) visited~)))))
 
 (define (redex-term l r visited)
   (cond
-    [(and (var? r) (not (var? l))) (values `(subst ,r ,(freshen l)) #f)]
+    [(and (var? r) (not (var? l))); Redex I
+     (values `(subst ,r ,(freshen l)) #f)]
     [(and (pair? l) (pair? r) (equal? (car l) (car r)))
      (redex-args l r visited)]
     [(var? l)
      (let ([prev-visit (assp (lambda (x) (var=? x l)) visited)])
-       (if prev-visit
-           ?
+       (if (and prev-visit (not (equal? (cdr prev-visit) r))); Redex II
+           (unify (cdr prev-visit)
            (values #f (cons `(,l . ,r) visited))))]
     [else (values #f visited)]))
 
