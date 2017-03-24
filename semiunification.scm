336ea691762795b0966b243122c9288d979549a4
Christopher Brooks <flavenstein@gmail.com>
2017-03-24 13:21:43 -0600

    Rewrite redex I, start over on everything else

--- semiunification.scm
+++ semiunification.scm
@@ -1,66 +1,21 @@
 (load "parse.scm")
-(load "examples.scm")
-
-(define (arity-0? term)
-  (and (list? term) (= (length term) 1) (atom? (car term))))
-
-(define (flatten term path)
-  (cond [(or (var? term) (arity-0? term))
-         (list (cons term path))]
-        [else
-         (let loop ([args (cdr term)] [i 0])
-           (if (null? args) '()
-               (append (flatten (car args) (append path (list (cons (car term) i))))
-                       (loop (cdr args) (+ i 1)))))]))
-
-(define (path-sort ps)
-  (sort
-    (lambda (x y)
-      (< (vector-ref (car x) 0)
-         (vector-ref (car y) 0)))
-    ps))
-
-(define (paths ineq)
-  (match ineq
-    [(< ,lhs ,rhs)
-     `(< ,(path-sort (flatten lhs '()))
-         ,(path-sort (flatten rhs '())))]))
-
-(define (explode SUP)
-  (match SUP
-    [() '()]
-    [(,ineq . ,ineqs)
-     (cons (paths ineq) (explode ineqs))]))
-
-(define (resolve-path flat-term path)
-  (if (null? path)
-      (if (null? (cdr flat-term))
-          (caar flat-term)
-          flat-term)
-      (resolve-path
-        (let loop ([term-paths flat-term] [first (car path)])
-          (cond [(null? term-paths) '()]
-                [(equal? first (cadar term-paths))
-                 (cons (cons (caar term-paths) (cddar term-paths))
-                       (loop (cdr term-paths) first))]
-                [else (loop (cdr term-paths) first)]))
-        (cdr path))))
 
 (define (freshen x) x)
 
-(define (redex-I lhs rhs)
-  (cond [(null? rhs) #f]
-        [(var? (caar rhs))
-         (let ([p (resolve-path lhs (cdar rhs))])
-           (if (var? p)
-               (redex-I lhs (cdr rhs))
-               (cons (caar rhs) (freshen lhs))))]
-        [else (redex-I lhs (cdr rhs))]))
+(define (redex-I-args l-args r-args)
+  (cond
+    [(and (null? l-args) (null? r-args)) #f?]
+    [(or (null? l-args) (null? r-args)) (error #f "Arity mismatch")]
+    [else (or (redex-I-term (car l-args) (car r-args))
+              (redex-I-args (cdr l-args) (cdr r-args)))]))
 
-(define (redex-II-2 ineq)
+(define (redex-I-term l r)
+  (cond
+    [(and (var r) (not (var l))) `(subst ,r ,(freshen l))]
+    [(and (pair? l) (pair? r) (equal? (car l) (car r)))
+     (redex-I-args l r)]
+    [else #f]))
+
+(define (redex-I ineq)
   (match ineq
-    [(< ,lhs ,rhs)
-      (match lhs
-        [(,a) #f]
-        [((,a . ,p1) (,a . ,p2) . ,tail) (cons p1 p2)]
-        [,_ (redex-II-2 `(< ,(cdr lhs) ,rhs))])]))
+    [(< ,lhs ,rhs) (redex-I-term lhs rhs)]))
