8ec7383f39013af83376d0e9f1443d81e671ce45
chris <cbrooks90@gmail.com>
2018-09-19 15:12:53 -0400

    We don't need a separate file for a small test macro

--- tests.scm
+++ tests.scm
@@ -1,5 +1,16 @@
-(load "miniKanren-wrappers.scm")
-(load "test-check.scm")
+(load "semiKanren.scm")
+
+(define-syntax test-check
+  (syntax-rules ()
+    ((_ title tested-expression expected-result)
+     (begin
+       (printf "Testing ~s\n" title)
+       (let* ((expected expected-result)
+              (produced tested-expression))
+         (or (equal? expected produced)
+             (errorf 'test-check
+                     "Failed: ~a~%Expected: ~a~%Computed: ~a~%"
+                     'tested-expression expected produced)))))))
 
 ;; Antiunification tests
 
